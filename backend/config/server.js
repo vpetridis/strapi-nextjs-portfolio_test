c22ca2242352868ef5c6a08cb75697b4c96c0fcc
vpetridis <vpetridis@modulus.gr>
2021-10-17 18:45:25 +0300

    Init commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c7232d28290631470e586217cb4b2d22'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cd39dcd5-c49c-484d-a45f-4e3e26ff16f3'
+};
